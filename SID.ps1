6789ba38070eb4bd3d2d63c6337c7ac8d58641fc
LeviHoward <levix.g.howard@intel.com>
2021-04-08 11:25:48 -0700

    add file

---
+++ SID.ps1
@@ -0,0 +1,11 @@
+﻿# find SID by using domain and IDSID
+$objUser = New-Object System.Security.Principal.NTAccount("amr", "lghowarx")
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+$strSID.Value
+
+# find IDSID by looking up SID
+$objSID = New-Object System.Security.Principal.SecurityIdentifier `
+    ("S-1-5-21-725345543-602162358-527237240-385223")
+$objUser = $objSID.Translate( [System.Security.Principal.NTAccount])
+$objUser.Value
+#edit
